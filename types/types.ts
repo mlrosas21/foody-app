de9d1ae9ad623d785fb9bd5513fcff5916a2daa6
Martin Rosas <mlrosas21@hotmail.com>
2023-09-20 12:04:22 -0300

    adding recipe types

---
+++ types/types.ts
@@ -0,0 +1,89 @@
+type Recipe = {
+  vegetarian: boolean;
+  vegan: boolean;
+  glutenFree: boolean;
+  dairyFree: boolean;
+  veryHealthy: boolean;
+  cheap: boolean;
+  veryPopular: boolean;
+  sustainable: boolean;
+  lowFodmap: boolean;
+  weightWatcherSmartPoints: number;
+  gaps: string;
+  preparationMinutes: number;
+  cookingMinutes: number;
+  aggregateLikes: number;
+  healthScore: number;
+  creditsText: string;
+  license: string;
+  sourceName: string;
+  pricePerServing: number;
+  extendedIngredients: Ingredient[];
+  id: number;
+  title: string;
+  readyInMinutes: number;
+  servings: number;
+  sourceUrl: string;
+  image: string;
+  imageType: string;
+  summary: string;
+  cuisines: string[];
+  dishTypes: string[];
+  diets: string[];
+  occasions: string[];
+  instructions: string;
+  analyzedInstructions: {
+    name: string;
+    steps: RecipeStep[];
+  }[];
+  originalId: any;
+  spoonacularSourceUrl: string;
+}
+
+type Ingredient = {
+  id: number;
+  aisle: string;
+  image: string;
+  consistency: string;
+  name: string;
+  nameClean: string;
+  original: string;
+  originalName: string;
+  amount: number;
+  unit: string;
+  meta: any[];
+  measures: {
+    us: {
+      amount: number;
+      unitShort: string;
+      unitLong: string;
+    };
+    metric: {
+      amount: number;
+      unitShort: string;
+      unitLong: string;
+    };
+  };
+}
+
+type RecipeStep = {
+  number: number;
+  step: string;
+  ingredients: {
+    id: number;
+    name: string;
+    localizedName: string;
+    image: string;
+  }[];
+  equipment: {
+    id: number;
+    name: string;
+    localizedName: string;
+    image: string;
+  }[];
+  length?: {
+    number: number;
+    unit: string;
+  }
+}
+
