14f0b8d121f9aaca60892cd60d63e69b7c6b0729
Martin Rosas <mlrosas21@hotmail.com>
2023-09-20 15:03:26 -0300

    + constants

---
+++ constants/recipe.ts
@@ -0,0 +1,8 @@
+export const RECIPE_FLAGS: { [key: string]: string } = {
+  glutenFree: 'Gluten-free',
+  vegan: 'Vegan',
+  vegetarian: 'Vegetarian',
+  dairyFree: 'Dairy-free',
+  veryPopular: 'Very popular',
+  sustainable: 'Sustainable'
+}
