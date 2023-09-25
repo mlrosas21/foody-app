4ce6e67c8a7faba0d7d562985a2d10b0c9facdaf
SE38606 <SE38606@grupo.ypf.com>
2023-09-25 16:37:17 -0300

    cleaning layout

--- src/app/recipes/[id]/layout.tsx
+++ src/app/recipes/[id]/layout.tsx
@@ -2,9 +2,8 @@ import { ReactNode } from "react";
 
 export default function RecipeLayout ({children}: {children: ReactNode}){
   return (
-    <div>
-      <h1>Recipe</h1>
+    <>
       {children}
-    </div>
+    </>
   )
 }
