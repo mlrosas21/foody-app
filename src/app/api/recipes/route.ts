ddfcaa249d1d1f60b87da5c7d0df40354a1321f2
mlrosas21 <mlrosas21@hotmail.com>
2023-09-27 15:08:01 -0300

    get route handler

---
+++ src/app/api/recipes/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+const API_URL = process.env.BASE_API_URL;
+const authString = `apiKey=${process.env.API_KEY}`;
+
+const fetchApi = async (endpoint: string, urlParams?: string) => {
+  const url = `${API_URL}/${endpoint}?${authString}&${urlParams || ""}`;
+  const response = await fetch(url);
+  return await response.json();
+};
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  if(!searchParams.has("number")) searchParams.set("number", '9')
+  
+  const recipes = await fetchApi(
+    `/recipes/complexSearch`,
+    searchParams.toString()
+  );
+
+  return NextResponse.json({recipes});
+}
