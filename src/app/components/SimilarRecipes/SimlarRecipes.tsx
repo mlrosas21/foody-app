2cbc06bdcdd9aa6745f1193af923f96e0331e24e
mlrosas21 <mlrosas21@hotmail.com>
2023-09-28 18:22:46 -0300

    box to paper

--- src/app/components/SimilarRecipes/SimlarRecipes.tsx
+++ src/app/components/SimilarRecipes/SimlarRecipes.tsx
@@ -1,21 +1,21 @@
 import { getSimilarRecipes } from "services/recipes";
 import styles from "./styles.module.css";
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import SimpleRecipeCard from "../SimpleRecipeCard/SimpleRecipeCard";
+import Paper from "@mui/material/Paper";
 
 const SimilarRecipes = async ({ id }: { id: number }) => {
   const recipes = await getSimilarRecipes(id);
 
   return (
-    <Box component={"aside"} className={styles.similarRecipes}>
+    <Paper component={"aside"} className={styles.similarRecipes}>
       <Typography gutterBottom variant="h5">
         Similar Recipes
       </Typography>
       {recipes.map((recipe) => (
         <SimpleRecipeCard recipe={recipe} />
       ))}
-    </Box>
+    </Paper>
   );
 };
 
